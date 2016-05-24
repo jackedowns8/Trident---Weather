f1ed978389ba5230804d92349ebd6ae54cc23cd6
Jack Downs <jack@downsy.co.nz>
2016-05-24 20:17:51 +1200

    Moved to onDeviceReady

--- www/index.js
+++ www/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+		console.log("initialize");
     },
     // Bind Event Listeners
     //
@@ -27,6 +28,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		console.log("bindEvents");
     },
     // deviceready Event Handler
     //
@@ -34,6 +36,10 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		console.log("deviceReady");
+		adbuddiz.setTestModeActive();
+			 adbuddiz.setAndroidPublisherKey("1ca0bda3-e6e0-4395-8255-333448265716");
+			 adbuddiz.cacheAds();
 		
     },
     // Update DOM on a Received Event
